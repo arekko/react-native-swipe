31f6e498c753724f182957f7429bc87b5ee21dd5
arekko <you@example.com>
2019-03-12 21:33:40 +0200

    initial commit

---
+++ src/Deck.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { Animated, Dimensions, PanResponder, View } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+
+export default class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY(0, 0);
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {}
+    });
+
+    this.state = { panResponder, position };
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ["-120deg", "0deg", "120deg"]
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
+  renderCards() {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+
+      return this.props.renderCard(item);
+    });
+  }
+
+  render() {
+    return <View>{this.renderCards()}</View>;
+  }
+}
